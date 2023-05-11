18f53df763dcfbdab0f7463eb4195759ea0ad1dd
Davi-Jesseh <98218418+Davi-Jesseh@users.noreply.github.com>
2023-05-11 00:26:28 -0300

    Perceptron_v1
    Perceptron funcionando mas ainda não usando os dados que foram dado no Slide.

---
+++ js/perceptron.js
@@ -0,0 +1,34 @@
+function sign(num){
+    return num >= 0 ? 1 : -1
+}
+
+class Perceptron {
+    weights = [0,0]
+
+    lr = 0.1
+
+    constructor(){
+        for (let i = 0; i < this.weights.length; i++){
+            this.weights[1] = random(-1, 1)
+        }
+    }
+
+    guess(inputs) {
+        let sum = 0
+        
+        for (let i = 0; i < this.weights.length; i++){
+            sum += inputs[i] * this.weights[i]
+        }
+        const output = sign(sum)
+        return output
+    }
+    train(inputs, target){
+        const guess = this.guess(inputs)
+
+        const error = target - guess
+
+        for(let i = 0; i < this.weights.length; i++) {
+            this.weights[i] += error * inputs[i] * this.lr
+        }
+    }
+}
